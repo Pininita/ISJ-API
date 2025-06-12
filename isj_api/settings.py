a542cacc7ee322261d71342804dab85779e5b344
Pininita <pininita92001@gmail.com>
2025-06-12 12:16:48 -0500

    csrf

--- isj_api/settings.py
+++ isj_api/settings.py
@@ -29,6 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
 
 # Application definition
 
