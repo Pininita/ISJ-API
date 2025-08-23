ae947c464d8d7e2b742bc73a33137ba240341a41
danielsanabria <pininita92001@gmail.com>
2025-08-23 09:09:15 -0500

    fixing error of static files to be able to preview graphql and admin route

--- isj_api/settings.py
+++ isj_api/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
 
 
 # Application definition
@@ -147,7 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <- importante
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- isj_api/urls.py
+++ isj_api/urls.py
@@ -19,9 +19,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('transactions.urls')),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ] + debug_toolbar_urls()
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
